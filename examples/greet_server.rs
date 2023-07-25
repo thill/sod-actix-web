32034c48797732c57d5e1aea5b1f8ea040da0b10
Eric Thill <code@thill.io>
2023-07-25 11:18:35 -0500

    initial commit

---
+++ examples/greet_server.rs
@@ -0,0 +1,26 @@
+use std::convert::Infallible;
+
+use actix_web::{web, App, HttpServer};
+use sod::Service;
+use sod_actix_web::ServiceHandler;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    struct GreetService;
+    impl Service<web::Path<String>> for GreetService {
+        type Output = String;
+        type Error = Infallible;
+        fn process(&self, name: web::Path<String>) -> Result<Self::Output, Self::Error> {
+            Ok(format!("Hello {name}!"))
+        }
+    }
+
+    HttpServer::new(|| {
+        App::new().service(
+            web::resource("/greet/{name}").route(web::get().to(ServiceHandler::new(GreetService))),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
